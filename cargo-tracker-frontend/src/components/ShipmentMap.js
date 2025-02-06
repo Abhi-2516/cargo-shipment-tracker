85eb1fae8435a29af1b6a090d7e9bea1b9883fdb
Abhi-2516 <127724654+Abhi-2516@users.noreply.github.com>
2025-02-06 23:13:58 +0530

    Add files via upload

---
+++ cargo-tracker-frontend/src/components/ShipmentMap.js
@@ -0,0 +1,15 @@
+// src/components/ShipmentMap.js
+
+import React from "react";
+
+const ShipmentMap = () => {
+    return (
+        <div className="shipment-map-container">
+            <h2>Map</h2>
+            {/* Implement your map component here */}
+            <p>Map will be displayed here (use libraries like react-leaflet).</p>
+        </div>
+    );
+};
+
+export default ShipmentMap;
